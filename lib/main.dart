0e55703fe4d8a0d1a9e6094f41cd4b8e12766529
Akshat Tandon <tandon.akshat@thinksys.com>
2019-06-27 20:34:12 +0530

    changes made

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+    backgroundColor: Colors.teal,
+    body: SafeArea(
+        child: Column(
+            children: <Widget>[
+          CircleAvatar(
+              backgroundColor: Colors.red,
+              radius: 50.0,
+              backgroundImage: AssetImage('images/cat.jpg')),
+          Text(
+            'Cute Cat',
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold,fontFamily: 'Pacifico'),
+          ),
+          Text(
+            'FLUTTER DEVLOPMENT',
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontFamily: 'SourceSansPro'),
+          ),
+        ])),
+  )));
 }
